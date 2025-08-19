bb1925f3f803c70c8cefae237ccc660b0e6222f5
Gauraw Kumar Maurya <gk42251@gmail.com>
2025-08-19 21:02:09 +0530

    Update GPIO_toggleoutput api

--- drivers/Src/stm32f407xx_gpio_driver.c
+++ drivers/Src/stm32f407xx_gpio_driver.c
@@ -284,7 +284,7 @@ void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint8_t Value)
 
  */
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber){
-
+	pGPIOx->ODR ^= (1 << PinNumber);
 }
 
 /*
